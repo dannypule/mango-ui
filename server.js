699c4a9276893b2319f92feded38a319db1aa2ee
Danny Pule <kojikusan@gmail.com>
2017-12-16 13:36:38 +0000

    refactoring folder structure

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const compression = require('compression');
 // const webpackDevMiddleware = require('webpack-dev-middleware'); // relevant to serverside rendering
 // const webpackHotMiddleware = require('webpack-hot-middleware'); // relevant to serverside rendering
 // const webpack = require('webpack');
-const App = require('./js/App').default; // need to put default as 'export default' returns an object with default key on it
+const App = require('./src/app/App').default; // need to put default as 'export default' returns an object with default key on it
 // const webpackConfig = require('./webpack.config'); // relevant to serverside rendering
 
 const StaticRouter = ReactRouter.StaticRouter;
@@ -37,9 +37,10 @@ const server = express();
 
 server.use(compression()); // does gzipping etc
 
-server.use('/public', express.static('./public')); // says: statically serve everything that is in the public directory and add correct mime types for images and do other things
+server.use('/public', express.static('./public')); // says: statically serve everything that is in the dist directory and add correct mime types for images and do other things
 
 server.use((req, res) => {
+  // res.send('foo');
   console.log(req.url);
   const context = {};
   const body = ReactDOMServer.renderToString(
--- src/app/App.jsx
+++ src/app/App.jsx
@@ -5,7 +5,7 @@ import PropTypes from 'prop-types';
 import { Provider } from 'react-redux';
 import store from './store';
 import AsyncRoute from './AsyncRoute';
-import preload from '../data.json';
+import preload from '../../data.json';
 
 const FourOhFour = () => <h1>404</h1>;
 
